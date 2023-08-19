df3d8402169b6bf3a9c4866cac3af8d2345d55eb
leoojg <leoojg@gmail.com>
2023-08-19 18:53:45 -0300

    feat: add nestjs config module to manage environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FarmerModule } from './farmer/farmer.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://mongo_db_user:mongo_db_password@localhost:27017/milk-hiring?authSource=admin',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const mongoDbUser = configService.getOrThrow('MONGO_DB_USER');
+        const mongoDbPassword = configService.getOrThrow('MONGO_DB_PASSWORD');
+        const mongoDbHost = configService.getOrThrow('MONGO_DB_HOST');
+        const mongoDbPort = configService.get('MONGO_DB_PORT') ?? 27017;
+        return {
+          uri: `mongodb://${mongoDbUser}:${mongoDbPassword}@${mongoDbHost}:${mongoDbPort}/milk-hiring?authSource=admin`,
+        };
+      },
+    }),
     FarmerModule,
   ],
   controllers: [],
